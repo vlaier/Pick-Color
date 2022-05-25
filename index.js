1acec0ed381ccf75b09c489d26f3d65b61ebd823
Piotr <piotrzielu.1996@gmail.com>
2022-05-25 17:53:44 +0200

    Add color swatch and some javascript funcionality

--- index.js
+++ index.js
@@ -2,20 +2,28 @@ const root = document.querySelector(":root");
 const colors = ["#ff96bd", "#9999fb", "#ffe797"];
 const menuButton = document.getElementsByClassName("hamburger")[0];
 const triangles = document.getElementById("triangles");
+const colorOptions = document.getElementsByClassName("option");
 let i = 0;
-const swatches = document.getElementsByClassName("colors");
+
+function changeColor(color) {
+  root.style.setProperty("--clr-active", color);
+  triangles.classList.add("active");
+  setTimeout(() => {
+    triangles.classList.remove("active");
+  }, 800);
+  console.log("clicked");
+}
+
 menuButton.addEventListener("click", () => {
   menuButton.classList.toggle("active");
 });
 triangles.addEventListener("click", () => {
   i += 1;
-  root.style.setProperty("--clr-active", colors[i % 3]);
-
-  triangles.classList.add("active");
-  setTimeout(() => {
-    triangles.classList.remove("active");
-  }, 800);
+  changeColor(colors[i % 3]);
 });
 for (let i = 0; i < 3; i++) {
-  swatches[0];
+  colorOptions[i].style.setProperty("fill", colors[i]);
+  colorOptions[i].addEventListener("click", () => {
+    changeColor(colors[i]);
+  });
 }
